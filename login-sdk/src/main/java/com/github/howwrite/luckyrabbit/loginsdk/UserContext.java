459bfc77a7fe7b881e510ee9e793d723254a97a7
howwrite <xwmdream@qq.com>
2021-09-27 23:07:10 +0800

    feature: init project

---
+++ login-sdk/src/main/java/com/github/howwrite/luckyrabbit/loginsdk/UserContext.java
@@ -0,0 +1,11 @@
+package com.github.howwrite.luckyrabbit.loginsdk;
+
+/**
+ * @author howwrite
+ * @date 2021/9/27 11:04 下午
+ */
+public class UserContext {
+    public static Long currentUserId() {
+        return 1L;
+    }
+}
